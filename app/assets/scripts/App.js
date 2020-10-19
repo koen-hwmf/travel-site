fc6a667c4c0dfa5d21b0fda19a8f4c3861dee53a
Koen <koenvanstraalen@gmail.com>
2020-10-19 09:51:32 +0200

    Modal voltooid.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,12 +7,27 @@ import StickyHeader from './modules/stickyHeader.js';
 const features = document.querySelectorAll('.feature');
 const testimonials = document.querySelectorAll('.testimonial');
 
-const stickyHeader = new StickyHeader();
-
+new StickyHeader();
 new MobileMenu();
-
 new RevealOnScroll(features, 75);
 new RevealOnScroll(testimonials, 60);
+let modal;
+
+document.querySelectorAll('.open-modal').forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (typeof modal === 'undefined') {
+      import(/* webpackChunkName: "modal" */ './modules/modal.js')
+        .then((modalData) => {
+          modal = new modalData.default();
+          setTimeout(() => modal.openModal(), 20);
+        })
+        .catch((e) => console.log(e));
+    } else {
+      modal.openModal();
+    }
+  });
+});
 
 if (module.hot) {
   module.hot.accept();
